d0581e95a1ee183e990a3eb87103812fc56c22c7
Europia79 <nikolai.kalashnikov@hotmail.com>
2014-06-03 11:28:51 -0500

    Added debugging dependency
    for
    modules/debug
    groupId - mc.euro
    artifactId - extraction-debug

--- modules/v1_5_R3/src/mc/euro/extraction/nms/v1_5_R3/Hostage.java
+++ modules/v1_5_R3/src/mc/euro/extraction/nms/v1_5_R3/Hostage.java
@@ -1,14 +1,18 @@
 package mc.euro.extraction.nms.v1_5_R3;
 
 import java.lang.reflect.Field;
+import mc.euro.extraction.debug.*;
 import net.minecraft.server.v1_5_R3.Entity;
+import net.minecraft.server.v1_5_R3.EntityCreature;
 import net.minecraft.server.v1_5_R3.EntityVillager;
 import net.minecraft.server.v1_5_R3.PathfinderGoalSelector;
 import net.minecraft.server.v1_5_R3.World;
 import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_5_R3.entity.CraftCreature;
 import org.bukkit.craftbukkit.v1_5_R3.entity.CraftVillager;
 import org.bukkit.craftbukkit.v1_5_R3.util.UnsafeList;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  *
@@ -16,25 +20,32 @@ import org.bukkit.entity.Player;
  */
 public class Hostage extends EntityVillager {
     
+    DebugInterface debug;
     String owner;
     int ownerID;
     World world2;
     
     public Hostage(World w) {
         super(w);
+        this.debug = new DebugOn((JavaPlugin) Bukkit.getPluginManager().getPlugin("HostageArena"));
         this.world2 = w;
         clearPathfinders();
         this.goalSelector.a(10, new PathfinderGoalFollowPlayer(this, 1.0F, 2.0F, 2.0F));
-        ((CraftVillager) super.getBukkitEntity()).setMaxHealth(20);
+        this.debug.log("MaxHealth = " + this.getMaxHealth());
+        this.debug.log("HP = " + this.getHealth());
+        ((CraftCreature) super.getBukkitEntity()).setMaxHealth(20);
         this.setHealth(20);
     }
     
     public Hostage(World w, int profession) {
         super(w, profession);
+        this.debug = new DebugOn((JavaPlugin) Bukkit.getPluginManager().getPlugin("HostageArena"));
         this.world2 = w;
         clearPathfinders();
-        this.goalSelector.a(10, new PathfinderGoalFollowPlayer(this, 1.0F, 2.0F, 2.0F)); 
-        ((CraftVillager) super.getBukkitEntity()).setMaxHealth(20);
+        this.goalSelector.a(10, new PathfinderGoalFollowPlayer(this, 1.0F, 2.0F, 2.0F));
+        this.debug.log("MaxHealth = " + this.getMaxHealth());
+        this.debug.log("HP = " + this.getHealth());
+        ((CraftCreature) super.getBukkitEntity()).setMaxHealth(20);
         this.setHealth(20);
     }
     
