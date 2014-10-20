fc6341c62bd53262bbfdd8d216d977feb3181f54
Europia79 <nikolai.kalashnikov@hotmail.com>
2014-10-19 21:00:22 -0500

    Added getRescuer()

--- modules/api/src/mc/euro/extraction/nms/Hostage.java
+++ modules/api/src/mc/euro/extraction/nms/Hostage.java
@@ -5,7 +5,26 @@ import org.bukkit.entity.Player;
 import org.bukkit.entity.Villager.Profession;
 
 /**
+ * CraftHostage implements Hostage. <br/><br/>
  * 
+ * <pre>
+ * Implementations: modules/{version}/CraftHostage.java
+ * 
+ * mc.euro.extraction.nms.pre.CraftHostage
+ * mc.euro.extraction.nms.v1_4_5.CraftHostage
+ * mc.euro.extraction.nms.v1_4_6.CraftHostage
+ * mc.euro.extraction.nms.v1_4_R1.CraftHostage
+ * mc.euro.extraction.nms.v1_5_R1.CraftHostage
+ * mc.euro.extraction.nms.v1_5_R2.CraftHostage
+ * mc.euro.extraction.nms.v1_5_R3.CraftHostage
+ * mc.euro.extraction.nms.v1_6_R1.CraftHostage
+ * mc.euro.extraction.nms.v1_6_R2.CraftHostage
+ * mc.euro.extraction.nms.v1_6_R3.CraftHostage
+ * mc.euro.extraction.nms.v1_7_R1.CraftHostage
+ * mc.euro.extraction.nms.v1_7_R2.CraftHostage
+ * mc.euro.extraction.nms.v1_7_R3.CraftHostage
+ * mc.euro.extraction.nms.v1_7_R4.CraftHostage
+ * </pre>
  * 
  * @author Nikolai
  */
@@ -29,5 +48,7 @@ public interface Hostage {
     public void setCustomName(String name);
     
     public void setHealth(double health);
+    
+    public Player getRescuer();
     
 }
