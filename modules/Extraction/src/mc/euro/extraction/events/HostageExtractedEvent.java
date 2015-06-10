525674579a83a8cf123043c69e08e67b567b72e0
Europia79 <nikolai.kalashnikov@hotmail.com>
2015-06-09 21:45:24 -0500

    Added getRescuer() to HostageExtractedEvent

--- modules/Extraction/src/mc/euro/extraction/events/HostageExtractedEvent.java
+++ modules/Extraction/src/mc/euro/extraction/events/HostageExtractedEvent.java
@@ -1,7 +1,8 @@
 package mc.euro.extraction.events;
 
+import mc.alk.arena.objects.ArenaPlayer;
 import mc.euro.extraction.nms.Hostage;
-import org.bukkit.entity.Player;
+
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
@@ -14,17 +15,19 @@ public class HostageExtractedEvent extends Event {
     
     private static final HandlerList handlers = new HandlerList();
     Hostage hostage;
+    ArenaPlayer rescuer;
     
-    public HostageExtractedEvent(Hostage h) {
+    public HostageExtractedEvent(Hostage h, ArenaPlayer rescuer) {
         this.hostage = h;
+        this.rescuer = rescuer;
     }
     
     public Hostage getHostage() {
         return this.hostage;
     }
     
-    public Player getRescuer() {
-        return this.hostage.getRescuer();
+    public ArenaPlayer getRescuer() {
+        return this.rescuer;
     }
 
     @Override
