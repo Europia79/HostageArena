77cac5f66fba9a0176034fcf948362ce704c3c88
Europia79 <nikolai.kalashnikov@hotmail.com>
2016-04-26 20:34:59 -0500

    Refactored
    refactored from a command executor specifically for type HostageArena to
    one more general to any type of ExtractionArena.

--- modules/Extraction/src/mc/euro/extraction/commands/ExtractionArenaCmdExecutor.java
+++ modules/Extraction/src/mc/euro/extraction/commands/ExtractionArenaCmdExecutor.java
@@ -1,15 +1,17 @@
 package mc.euro.extraction.commands;
 
 import java.util.List;
+
 import mc.alk.arena.BattleArena;
 import mc.alk.arena.executors.CustomCommandExecutor;
 import mc.alk.arena.executors.MCCommand;
 import mc.alk.arena.objects.arenas.Arena;
-import mc.euro.extraction.HostageArena;
 import mc.euro.extraction.api.IHostagePlugin;
+import mc.euro.extraction.arenas.ExtractionArena;
 import mc.euro.extraction.debug.*;
 import mc.euro.extraction.nms.Hostage;
 import mc.euro.extraction.nms.NPCFactory;
+
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.command.CommandSender;
@@ -19,21 +21,21 @@ import org.bukkit.entity.Player;
  *
  * @author Nikolai
  */
-public class HostageExecutor extends CustomCommandExecutor {
+public class ExtractionArenaCmdExecutor extends CustomCommandExecutor {
     
     IHostagePlugin plugin;
     
-    public HostageExecutor(IHostagePlugin p) {
+    public ExtractionArenaCmdExecutor(IHostagePlugin p) {
         this.plugin = p;
     }
     
     @MCCommand(cmds={"list"},subCmds={"extractionpoint","extractionpoints"}, perm="vips.extractionpoints.list")
     public boolean listExtractionPoints(CommandSender sender, Arena a) {
-        if (!(a instanceof HostageArena)) {
-            sender.sendMessage("" + a.getName() + " is not a HostageArena");
+        if (!(a instanceof ExtractionArena)) {
+            sender.sendMessage("" + a.getName() + " is not an ExtractionArena");
             return false;
         }
-        HostageArena arena = (HostageArena) a;
+        ExtractionArena arena = (ExtractionArena) a;
         List<Location> locs = arena.getExtractionPoints();
         if (locs.size() == 0) {
             sender.sendMessage("This arena (" + a.getName() + ") does not have any extraction points");
@@ -57,25 +59,26 @@ public class HostageExecutor extends CustomCommandExecutor {
     @MCCommand(cmds={"clear"}, subCmds={"extractionpoints"}, perm="vips.extractionpoints.clear")
     public boolean clearExtractionPoints(CommandSender sender, Arena a) {
         // path = arenas.{arenaName}.persistables.epoints
-        if (!(a instanceof HostageArena)) {
-            sender.sendMessage("" + a.getName() + " is not a valid HostageArena.");
+        if (!(a instanceof ExtractionArena)) {
+            sender.sendMessage("" + a.getName() + " is not a valid ExtractionArena.");
             return false;
         }
-        HostageArena arena = (HostageArena) a;
+        ExtractionArena arena = (ExtractionArena) a;
         arena.clearExtractionPoints();
         BattleArena.saveArenas(plugin);
         String msg = "All extraction points for arena " + a.getName() + " have been deleted.";
+        sender.sendMessage(msg);
         return true;
     }
 
     @MCCommand(cmds = {"add"}, subCmds = {"extractionpoint"}, perm="vips.extractionpoints.add")
     public boolean addExtractionPoint(Player sender, Arena a) {
         
-        if (!(a instanceof HostageArena)) {
-            sender.sendMessage("Arena must be a valid HostageArena.");
+        if (!(a instanceof ExtractionArena)) {
+            sender.sendMessage("Arena must be a valid ExtractionArena.");
             return false;
         }
-        HostageArena arena = (HostageArena) a;
+        ExtractionArena arena = (ExtractionArena) a;
         Location loc = sender.getLocation();
         arena.addExtractionPoint(loc);
         BattleArena.saveArenas(plugin);
@@ -85,7 +88,7 @@ public class HostageExecutor extends CustomCommandExecutor {
     
     public boolean addExtractionZone(Player sender, Arena a) {
         /*
-        HostageArena arena = (HostageArena) arena;
+        ExtractionArena arena = (ExtractionArena) arena;
         if (Bukkit.getPluginManager().isPluginEnabled("WorldEdit")) {
             WorldEditPlugin wePlugin = (WorldEditPlugin) Bukkit
                     .getPluginManager().getPlugin("WorldEdit");
@@ -175,11 +178,11 @@ public class HostageExecutor extends CustomCommandExecutor {
     public boolean toggleDebug(CommandSender sender) {
         if (plugin.debug() instanceof DebugOn) {
             plugin.setDebugging(false);
-            sender.sendMessage("Debugging mode for the HostageArena has been turned off.");
+            sender.sendMessage("Debugging mode for HostageArenas has been turned off.");
             return true;
         } else {
             plugin.setDebugging(true);
-            sender.sendMessage("Debugging mode for the HostageArena has been turned on.");
+            sender.sendMessage("Debugging mode for HostageArenas has been turned on.");
             return true;
         }
     }
