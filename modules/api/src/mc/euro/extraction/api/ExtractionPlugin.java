e26ac8fb787b8dff1fd1942dca7bd7041edd3d74
Europia79 <nikolai.kalashnikov@hotmail.com>
2016-04-30 17:02:50 -0500

    Removed un-used CustomConfig getter

--- modules/api/src/mc/euro/extraction/api/ExtractionPlugin.java
+++ modules/api/src/mc/euro/extraction/api/ExtractionPlugin.java
@@ -1,19 +1,17 @@
 package mc.euro.extraction.api;
 
-import mc.euro.extraction.appljuze.CustomConfig;
 import mc.euro.extraction.debug.DebugInterface;
+
 import org.bukkit.plugin.Plugin;
 
 /**
  *
  * @author Nikolai
  */
-public interface IHostagePlugin extends Plugin {
+public interface ExtractionPlugin extends Plugin {
     
     public DebugInterface debug();
     public boolean toggleDebug();
     public void setDebugging(boolean enable);
-    
-    public CustomConfig getConfig(String fileName);
     
 }
