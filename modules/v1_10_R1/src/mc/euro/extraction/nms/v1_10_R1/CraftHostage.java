12ca05755ecd6668c6695f0fda615da83b5869cc
Europia79 <nikolai.kalashnikov@hotmail.com>
2016-06-19 10:37:32 -0500

    Added modules/v1_10_R1

---
+++ modules/v1_10_R1/src/mc/euro/extraction/nms/v1_10_R1/CraftHostage.java
@@ -0,0 +1,167 @@
+package mc.euro.extraction.nms.v1_10_R1;
+
+import com.google.common.collect.Sets;
+
+import java.lang.reflect.Field;
+import java.util.UUID;
+
+import mc.euro.extraction.nms.Hostage;
+
+import net.minecraft.server.v1_10_R1.Entity;
+import net.minecraft.server.v1_10_R1.EntityAgeable;
+import net.minecraft.server.v1_10_R1.EntityOwnable;
+import net.minecraft.server.v1_10_R1.EntityPlayer;
+import net.minecraft.server.v1_10_R1.EntityVillager;
+import net.minecraft.server.v1_10_R1.PathfinderGoalSelector;
+import net.minecraft.server.v1_10_R1.World;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.craftbukkit.v1_10_R1.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Villager.Profession;
+
+/**
+ *
+ * @author Nikolai
+ */
+public class CraftHostage extends EntityVillager implements EntityOwnable, Hostage {
+    
+    private String owner;
+    private String lastOwner;
+    
+    public CraftHostage(World w) {
+        super(w);
+        clearPathfinders();
+        this.goalSelector.a(10, new PathfinderGoalFollowPlayer(this, 1.0D, 2.0F, 2.0F));
+    }
+    
+    public CraftHostage(World w, int profession) {
+        super(w, profession);
+        clearPathfinders();
+        this.goalSelector.a(10, new PathfinderGoalFollowPlayer(this, 1.0D, 2.0F, 2.0F));
+    }
+    
+    private void clearPathfinders() {
+        try {
+            Field bField = PathfinderGoalSelector.class.getDeclaredField("b");
+            bField.setAccessible(true);
+            Field cField = PathfinderGoalSelector.class.getDeclaredField("c");
+            cField.setAccessible(true);
+            bField.set(goalSelector, Sets.newLinkedHashSet());
+            bField.set(targetSelector, Sets.newLinkedHashSet());
+            cField.set(goalSelector, Sets.newLinkedHashSet());
+            cField.set(targetSelector, Sets.newLinkedHashSet());
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        }
+    }
+    
+    @Override
+    public void stay() {
+        this.lastOwner = owner;
+        this.owner = null;
+    }
+    
+    @Override
+    public boolean isStopped() {
+        return this.owner == null;
+    }
+    
+    @Override
+    public boolean isFollowing() {
+        return this.owner != null;
+    }
+    
+    @Override
+    public void follow(Player p) {
+        follow(p.getName());
+    }
+    
+    @Override
+    public void follow(String p) {
+        this.owner = p;
+    }
+    
+    @Override
+    public void setOwner(Player p) {
+        setOwner(p.getName());
+    }
+    
+    @Override
+    public void setOwner(String name) {
+        this.owner = name;
+    }
+
+    @Override
+    public String getOwnerName() {
+        return this.owner;
+    }
+
+    @Override
+    public Entity getOwner() {
+        if (this.owner == null) return null;
+        Player player = Bukkit.getPlayer(this.owner);
+        EntityPlayer ep = ((CraftPlayer)player).getHandle();
+        return ep;
+    }
+    
+    @Override
+    public UUID getOwnerUUID() {
+        if (this.owner == null) {
+            return null;
+        }
+        return getOwner().getUniqueID();
+    }
+    
+    @Override
+    public Location getLocation() {
+        Location loc = new Location(world.getWorld(), locX, locY, locZ, yaw, pitch);
+        return loc;
+    }
+    
+    @Override
+    public void setLocation(Location loc) {
+        double x = loc.getX();
+        double y = loc.getY();
+        double z = loc.getZ();
+        float newYaw = loc.getYaw();
+        float newPitch = loc.getPitch();
+        setLocation(x, y, z, newYaw, newPitch);
+    }
+
+    @Override
+    public void removeEntity() {
+        world.removeEntity(this);
+    }
+
+    @Override
+    public EntityAgeable createChild(EntityAgeable ea) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Profession getProfessionType() {
+        int id = getProfession();
+        return Profession.getProfession(id);
+    }
+
+    @Override
+    public void setProfessionType(Profession x) {
+        setProfession(x.getId());
+    }
+
+    @Override
+    public void setHealth(double health) {
+        setHealth((float) health);
+    }
+    
+    @Override
+    public Player getRescuer() {
+        String name = (owner == null) ? lastOwner : owner;
+        if (name == null) return null;
+        Player rescuer = Bukkit.getPlayer(name);
+        return rescuer;
+    }
+    
+}
---
+++ modules/v1_10_R1/src/mc/euro/extraction/nms/v1_10_R1/PathfinderGoalFollowPlayer.java
@@ -0,0 +1,121 @@
+package mc.euro.extraction.nms.v1_10_R1;
+
+import net.minecraft.server.v1_10_R1.BlockPosition;
+import net.minecraft.server.v1_10_R1.EntityHuman;
+import net.minecraft.server.v1_10_R1.EntityLiving;
+import net.minecraft.server.v1_10_R1.IBlockData;
+import net.minecraft.server.v1_10_R1.Material;
+import net.minecraft.server.v1_10_R1.MathHelper;
+import net.minecraft.server.v1_10_R1.Navigation;
+import net.minecraft.server.v1_10_R1.NavigationAbstract;
+import net.minecraft.server.v1_10_R1.PathType;
+import net.minecraft.server.v1_10_R1.PathfinderGoal;
+import net.minecraft.server.v1_10_R1.World;
+
+/**
+ * Modified PathfinderGoalFollowOwner. <br/><br/>
+ *
+ * @author Nikolai
+ */
+public class PathfinderGoalFollowPlayer extends PathfinderGoal {
+
+    private final CraftHostage d;
+    private EntityLiving e;
+    World a;
+    private final double f;
+    private final NavigationAbstract g;
+    private int h;
+    float b;
+    float c;
+    private float i;
+
+    public PathfinderGoalFollowPlayer(CraftHostage paramEntityTameableAnimal, double paramDouble, float paramFloat1, float paramFloat2) {
+        this.d = paramEntityTameableAnimal;
+        this.a = paramEntityTameableAnimal.world;
+        this.f = paramDouble;
+        this.g = paramEntityTameableAnimal.getNavigation();
+        this.c = paramFloat1;
+        this.b = paramFloat2;
+        a(3);
+        if (!(paramEntityTameableAnimal.getNavigation() instanceof Navigation)) {
+            throw new IllegalArgumentException("Unsupported mob type for FollowOwnerGoal");
+        }
+    }
+
+    @Override
+    public boolean a() {
+        if (!(this.d.getOwner() instanceof EntityLiving)) {
+            return false;
+        }
+        EntityLiving localEntityLiving = (EntityLiving) this.d.getOwner();
+        if (localEntityLiving == null) {
+            return false;
+        }
+        if (((localEntityLiving instanceof EntityHuman)) && (((EntityHuman) localEntityLiving).isSpectator())) {
+            return false;
+        }
+        if (this.d.h(localEntityLiving) < this.c * this.c) {
+            return false;
+        }
+        this.e = localEntityLiving;
+        return true;
+    }
+
+    @Override
+    public boolean b() {
+        return (!this.g.n()) && (this.d.h(this.e) > this.b * this.b);
+    }
+
+    public void c() {
+        this.h = 0;
+        this.i = this.d.a(PathType.WATER);
+        this.d.a(PathType.WATER, 0.0F);
+    }
+
+    @Override
+    public void d() {
+        this.e = null;
+        this.g.o();
+        this.d.a(PathType.WATER, this.i);
+    }
+
+    private boolean a(BlockPosition paramBlockPosition) {
+        IBlockData localIBlockData = this.a.getType(paramBlockPosition);
+        if (localIBlockData.getMaterial() == Material.AIR) {
+            return true;
+        }
+        return !localIBlockData.h();
+    }
+
+    @Override
+    public void e() {
+        this.d.getControllerLook().a(this.e, 10.0F, this.d.N());
+        if (--this.h > 0) {
+            return;
+        }
+        this.h = 10;
+        if (this.g.a(this.e, this.f)) {
+            return;
+        }
+        if (this.d.isLeashed()) {
+            return;
+        }
+        if (this.d.h(this.e) < 144.0D) {
+            return;
+        }
+        int j = MathHelper.floor(this.e.locX) - 2;
+        int k = MathHelper.floor(this.e.locZ) - 2;
+        int m = MathHelper.floor(this.e.getBoundingBox().b);
+        for (int n = 0; n <= 4; n++) {
+            for (int i1 = 0; i1 <= 4; i1++) {
+                if ((n < 1) || (i1 < 1) || (n > 3) || (i1 > 3)) {
+                    if ((this.a.getType(new BlockPosition(j + n, m - 1, k + i1)).q()) && (a(new BlockPosition(j + n, m, k + i1))) && (a(new BlockPosition(j + n, m + 1, k + i1)))) {
+                        this.d.setPositionRotation(j + n + 0.5F, m, k + i1 + 0.5F, this.d.yaw, this.d.pitch);
+                        this.g.o();
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
